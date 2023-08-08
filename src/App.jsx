532f15bc7a948afb034a63411ef2c94e87c136cf
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-08-08 16:06:24 +0200

    most of functionality is done, there is a bug that I need to deal with

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 
 function App() {
   const [theme, setTheme] = useState("dark");
+  const [message, setMessage] = useState(null);
+  const [value, setValue] = useState("");
+  const [currentTitle, setCurrentTitle] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
 
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
@@ -14,31 +18,126 @@ function App() {
     document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
+  useEffect(() => {
+    if (value && message) {
+      setCurrentTitle(value);
+    }
+  }, [message]);
+
+  useEffect(() => {
+    if (currentTitle && message) {
+      setPreviousChats((previousChats) => [
+        ...previousChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:3001/chatgpt/completions",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+  const currentChat = previousChats.filter(
+    (previousChats) => previousChats.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  );
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  };
+
+  console.log(message);
+  console.log(uniqueTitles);
+  console.log(previousChats);
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
-        <ul className="history"></ul>
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
+        </ul>
         <nav>
-          <a>https://rodzenpiotr.com</a>
+          <a href="https://rodzenpiotr.com">https://rodzenpiotr.com</a>
         </nav>
       </section>
       <section className="main">
-        <h1>Tech Pietrek GPT</h1>
-        <ul className="feed"></ul>
-      </section>
-      <div className="bottom-section">
-        <div className="input-container">
-          <input />
-          <div id="submit">-a</div>
-          <p className="info">
-            ChatGPT may produce inaccurate information about people, places, or
-            facts. ChatGPT August 3 Version
-          </p>
+        {!currentTitle && <h1>Tech Pietrek GPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>{" "}
+            </li>
+          ))}
+        </ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="none"
+                className="h-4 w-4 m-1 md:m-0"
+                strokeWidth="2"
+              >
+                <path
+                  d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+            <p className="info">
+              ChatGPT may produce inaccurate information about people, places,
+              or facts. ChatGPT August 3 Version
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
       <div>
-        <button onClick={toggleTheme}>Toggle Mode</button>
+        <button onClick={toggleTheme}>Toggle</button>
       </div>
     </div>
   );
